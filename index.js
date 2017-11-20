daf612c69bcab5d035b194917c65ce5fb1c17bba
Schoonology <michael.r.schoonmaker@gmail.com>
2017-11-20 17:54:06 -0500

    Export mapper.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var createClient = require('./lib/client')
+var mapper = require('./lib/mapper')
 
 module.exports = createClient('https://www.boardgamegeek.com/xmlapi2')
 module.exports.client = createClient
+module.exports.mapper = mapper
